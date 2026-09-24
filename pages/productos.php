7486e92bae60dc4f79455869310b48c77e6f3a15
César André <gonzales_cesar_24@institutovrht.edu.pe>
2026-09-24 17:03:28 -0500

    Fix: agrega require Configuracion en productos.php

--- pages/productos.php
+++ pages/productos.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/../config.php';
 require_once __DIR__ . '/../middleware/Auth.php';
+require_once __DIR__ . '/../models/Configuracion.php';
 require_once __DIR__ . '/../controllers/ProductoController.php';
 require_once __DIR__ . '/../controllers/CategoriaController.php';
 
